5a9677cf67c2c7bfdd0a2477270d35034eafc65b
jybapi <Administrator@DESKTOP-2TKU0DS>
2020-10-09 23:27:44 +0700

    添加经纪人银行卡页面

---
+++ Elight.Logic/Sys/SysAgentBankLogic.cs
@@ -0,0 +1,173 @@
+﻿using Elight.Entity.Sys;
+using Elight.Logic.Base;
+using Elight.Utility.Log;
+using Elight.Utility.Model;
+using Elight.Utility.Operator;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using SqlSugar;
+using System.Linq;
+
+namespace Elight.Logic.Sys
+{
+    public class SysAgentBankLogic : BaseLogic
+    {
+        /// <summary>
+        /// 经纪人银行卡分页
+        /// </summary>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        public List<SysAgentBankEntity> GetAgentBankPage(PageParm parm, ref int totalCount)
+        {
+            var result = new List<SysAgentBankEntity>();
+            try
+            {
+                if (parm == null)
+                {
+                    parm = new PageParm();
+                }
+                Dictionary<string, object> dic = new Dictionary<string, object>();
+                if (!string.IsNullOrEmpty(parm.where))
+                {
+                    dic = JsonConvert.DeserializeObject<Dictionary<string, object>>(parm.where);
+                }
+                using (var db = GetSqlSugarDB(DbConnType.QPAgentAnchorDB))
+                {
+                    return db.Queryable<SysAgentBankEntity, SysUser>((it, st) => new object[] { JoinType.Left, it.AgentID == st.Id })
+                        .Where((it, st) => st.ShopID == OperatorProvider.Instance.Current.ShopID)
+                        .WhereIF(dic.ContainsKey("Name") && !string.IsNullOrEmpty(dic["Name"].ToString()), (it, st) => st.Account.Contains(dic["Name"].ToString()))
+                                    .Select((it, st) => new SysAgentBankEntity
+                                    {
+                                        id = it.id,
+                                        AgentName = st.Account,
+                                        address = it.address,
+                                        bankaccount = it.bankaccount,
+                                        bankano = it.bankano,
+                                        CategoryCode = it.CategoryCode,
+                                        createtime = it.createtime,
+                                        payType = it.payType
+                                    })
+                                    .ToPageList(parm.page, parm.limit, ref totalCount);
+                }
+            }
+            catch (Exception ex)
+            {
+                new LogLogic().Write(Level.Error, "经纪人银行卡分页", ex.Message, ex.StackTrace);
+            }
+            return result;
+        }
+        /// <summary>
+        /// 根据主键得到商户信息
+        /// </summary>
+        /// <param name="primaryKey"></param>
+        /// <returns></returns>
+        public SysAgentBankEntity Get(int primaryKey)
+        {
+            using (var db = GetInstance())
+            {
+                return db.Queryable<SysAgentBankEntity, SysUser>((A, B) => new Object[] { JoinType.Left, A.AgentID == B.Id })
+                    .Where((A) => A.id == primaryKey)
+                    .Select((A, B) => new SysAgentBankEntity
+                    {
+                        id = A.id,
+                        AgentName = B.Account,
+                        address = A.address,
+                        bankaccount = A.bankaccount,
+                        bankano = A.bankano,
+                        CategoryCode = A.CategoryCode,
+                        payType = A.payType
+                    })
+                    .First();
+            }
+        }
+        /// <summary>
+        /// 新增经纪人银行卡
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int Insert(SysAgentBankEntity model)
+        {
+            try
+            {
+                using (var db = GetInstance())
+                {
+                    model.createtime = DateTime.Now;
+                    return db.Insertable(model).ExecuteReturnIdentity();
+                }
+            }
+            catch (Exception ex)
+            {
+                new LogLogic().Write(Level.Error, "新增商户", ex.Message, ex.StackTrace);
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// 更新经纪人银行卡
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int Update(SysAgentBankEntity model)
+        {
+            try
+            {
+                using (var db = GetInstance())
+                {
+                    return db.Updateable<SysAgentBankEntity>().SetColumns(it => new SysAgentBankEntity
+                    {
+                        bankano = model.bankano,
+                        CategoryCode = model.CategoryCode,
+                        payType = model.payType,
+                        bankaccount = model.bankaccount,
+                        address = model.address
+                    }).Where(it => it.id == model.id).ExecuteCommand();
+                }
+            }
+            catch (Exception ex)
+            {
+                new LogLogic().Write(Level.Error, "更新经纪人银行卡", ex.Message, ex.StackTrace);
+            }
+            return 0;
+        }
+        /// <summary>
+        /// 批量删除银行卡信息
+        /// </summary>
+        /// <param name="primaryKeys"></param>
+        /// <returns></returns>
+        public int Delete(List<long> idList)
+        {
+            using (var db = GetInstance())
+            {
+                return db.Deleteable<SysAgentBankEntity>().In(idList).ExecuteCommand();
+            }
+        }
+        /// <summary>
+        /// 经纪人银行卡下拉框
+        /// </summary>
+        /// <param name="id">经纪人id</param>
+        /// <returns></returns>
+        public string GetUserBankSelect(string id)
+        {
+            var result = "";
+            try
+            {
+                using (var db = GetInstance())
+                {
+                    result = db.Queryable<SysAgentBankEntity>()
+                        .Where(it => it.AgentID == id)
+                                .Select((it) => new
+                                {
+                                    BankName = it.CategoryCode + "--" + it.bankano,
+                                    it.id,
+                                }).ToJson();
+                }
+            }
+            catch (Exception ex)
+            {
+                new LogLogic().Write(Level.Error, "经纪人银行卡下拉框", ex.Message, ex.StackTrace);
+            }
+            return result;
+        }
+    }
+}
---
+++ Elight.WebUI/Areas/System/Controllers/AgentBankController.cs
@@ -0,0 +1,118 @@
+﻿using Elight.Entity.Sys;
+using Elight.Logic.Sys;
+using Elight.Utility.Extension;
+using Elight.Utility.Format;
+using Elight.Utility.Model;
+using Elight.Utility.ResponseModels;
+using Elight.WebUI.Controllers;
+using Elight.WebUI.Filters;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Elight.WebUI.Areas.System.Controllers
+{
+    [LoginChecked]
+    public class AgentBankController : BaseController
+    {
+        private readonly SysAgentBankLogic sysAgentBankLogic;
+        public AgentBankController()
+        {
+            sysAgentBankLogic = new SysAgentBankLogic();
+        }
+        [HttpGet, AuthorizeChecked]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 获取代理银行卡
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="keyWord">查询条件</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult GetAgentBankPage(PageParm parm)
+        {
+            int totalCount = 0;
+            var res = sysAgentBankLogic.GetAgentBankPage(parm, ref totalCount);
+            return pageSuccess(res, totalCount);
+        }
+        /// <summary>
+        /// 编辑页面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, AuthorizeChecked]
+        public ActionResult Form()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新增或修改
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost, AuthorizeChecked, ValidateAntiForgeryToken]
+        public ActionResult Form(SysAgentBankEntity model)
+        {
+            if (model.id == 0)
+            {
+                var agentModel = new SysUserLogic().CheckUserName(model.AgentName);
+                if (agentModel == null)
+                {
+                    return Error("经纪人不存在!");
+                }
+                model.AgentID = agentModel.Id;
+                model.ShopID = agentModel.ShopID;
+                int row = sysAgentBankLogic.Insert(model);
+                return row > 0 ? Success() : Error();
+            }
+            else
+            {
+                int row = sysAgentBankLogic.Update(model);
+                return row > 0 ? Success() : Error();
+            }
+        }
+        /// <summary>
+        /// 根据ID获取用户信息
+        /// </summary>
+        /// <param name="primaryKey"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult GetForm(int primaryKey)
+        {
+            SysAgentBankEntity entity = sysAgentBankLogic.Get(primaryKey);
+            return Content(entity.ToJson());
+        }
+        /// <summary>
+        /// 批量删除
+        /// </summary>
+        /// <param name="primaryKey"></param>
+        /// <returns></returns>
+        [HttpPost, AuthorizeChecked]
+        public ActionResult Delete(string primaryKey)
+        {
+            string[] idArr = primaryKey.ToStrArray();
+            List<long> idlist = new List<long>();
+            idArr.ForEach(it =>
+            {
+                idlist.Add(Convert.ToInt64(it));
+            });
+            int row = sysAgentBankLogic.Delete(idlist);
+            return row > 0 ? Success() : Error();
+        }
+        /// <summary>
+        /// 经纪人银行卡
+        /// </summary>
+        /// <returns>new {id,username}</returns>
+        [HttpGet]
+        public string GetUserBankSelect(string id)
+        {
+            return sysAgentBankLogic.GetUserBankSelect(id);
+        }
+    }
+}
